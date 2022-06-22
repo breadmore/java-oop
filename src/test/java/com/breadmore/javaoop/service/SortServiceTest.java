7fe437c0a10e00ddc09eeafcf782ef2e0cbd2e36
BM <lepo3@naver.com>
2022-06-22 19:39:11 +0900

    Implement #3 - SortService
    정렬 구현체를 사용하는 서비스를 별도로 작성
    의존성 주입 방법 적용

---
+++ src/test/java/com/breadmore/javaoop/service/SortServiceTest.java
@@ -0,0 +1,24 @@
+package com.breadmore.javaoop.service;
+
+import com.breadmore.javaoop.logic.BubbleSort;
+import com.breadmore.javaoop.logic.JavaSort;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortServiceTest {
+
+    private SortService sut = new SortService(new BubbleSort<>());
+
+    @Test
+    void test(){
+
+        List<String> actual = sut.doSort(List.of("3", "2", "1"));
+
+        assertEquals(List.of("1","2","3"),actual);
+
+    }
+
+}
