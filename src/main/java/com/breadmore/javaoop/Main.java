37177e3a2582422801cdc68daafb2dad325f223f
BM <lepo3@naver.com>
2022-06-22 19:39:46 +0900

    Update #3 - Main
    변수 클래스를 인터페이스로 변경
    #3 종료

--- src/main/java/com/breadmore/javaoop/Main.java
+++ src/main/java/com/breadmore/javaoop/Main.java
@@ -1,12 +1,14 @@
 package com.breadmore.javaoop;
 
 import com.breadmore.javaoop.logic.BubbleSort;
+import com.breadmore.javaoop.logic.JavaSort;
+import com.breadmore.javaoop.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
 
         System.out.println("[result] " + sort.sort(Arrays.asList(args)));
     }
